78508d4682428ced79a979e9332ce6d233eacbbd
vinayakshahdeo <vinushahdeo3333@gmail.com>
2024-02-22 13:53:36 +0530

    fe hooks

---
+++ front-end/src/auth/useUser.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import { useToken } from './useToken';
+const useUser = () => {
+	const [token] = useToken();
+
+	const getPayloadFromToken = (token) => {
+		const encodedPayload = (token.payload = token.split('.')[1]);
+		return JSON.parse(atob(encodedPayload));
+	};
+
+	const [user, setUser] = useState(() => {
+		if (!token) return null;
+		return getPayloadFromToken(token);
+	});
+
+	useEffect(() => {
+		if (!token) setUser(null);
+		setUser(getPayloadFromToken(token));
+	}, [token]);
+
+	return user;
+};
+
+export default useUser;
